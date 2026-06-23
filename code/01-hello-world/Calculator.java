71358f5ce8facef4a1db16f478a96c0ff3586b64
zoe <2742953635@qq.com>
2026-06-23 19:02:50 +0800

    第一课：变量与数据类型、运算符

---
+++ code/01-hello-world/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator{
+	public static void main(String[] args){
+		System.out.println(17/4);
+		System.out.println(17.0/4);
+		System.out.println(17%4);
+		System.out.println(15 > 20);
+	}
+}
