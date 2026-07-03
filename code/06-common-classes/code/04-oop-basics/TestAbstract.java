9c7bd0deb8e82dc5b330226db17bc45f88081aad
zoe <2742953635@qq.com>
2026-07-04 00:15:04 +0800

    删除错误目录

--- code/06-common-classes/code/04-oop-basics/TestAbstract.java
+++
@@ -1,9 +0,0 @@
-public class TestAbstract {
-    public static void main(String[] args) {
-        Animal dog = new Dog("旺财");
-        Animal cat = new Cat("咪咪");
-        
-        dog.makeSound();
-        cat.makeSound();
-    }
-}
